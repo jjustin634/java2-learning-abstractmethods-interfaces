3b767894c1e163be3661a0a72fc8d3413ccb5daf
Justin Johnson <jjustin634@gmail.com>
2018-11-13 14:54:04 -0600

    Updated my TestTaker class - added a try catch / and main print println methods

--- Main/src/com/jsoftware/api/TestTaker.java
+++ Main/src/com/jsoftware/api/TestTaker.java
@@ -1,14 +1,49 @@
 package com.jsoftware.api;
 
+import java.util.Scanner;
+
 public class TestTaker {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
         log("Welcome to the TestTaker program!");
         log("What test would you like to take? ");
+        String testLoading = input.next();
+
+        // use the load method to load the test the user created
         log("Test loaded successful!");
+
+        log("Enter 1 to take a whole test or 2 to take a sample test.");
+        logger("Your choice: ");
+        int wholeOrSampleTest = input.nextInt();
+
+        if (wholeOrSampleTest == 1) {
+            wholeTest();
+        } else {
+            sampleTest();
+        }
+    }
+
+    public static void wholeTest() {
+        log("----------------------------\n" +
+                "The test starts now!\n" +
+                "----------------------------");
+
+    }
+
+    public static void sampleTest() {
+        log("----------------------------\n" +
+                "The test starts now!\n" +
+                "----------------------------");
+        
     }
 
     public static void log(String m) {
         System.out.println(m);
+    }
+
+    public static void logger(String m) {
+        System.out.print(m);
     }
 }
