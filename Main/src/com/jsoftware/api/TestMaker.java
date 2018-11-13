654984eeb069b15daffc4f7699978c55fb23dbed
Justin Johnson <jjustin634@gmail.com>
2018-11-13 10:02:21 -0600

    Added a method called log and logger as a shorthand to println and print throughout my program

--- Main/src/com/jsoftware/api/TestMaker.java
+++ Main/src/com/jsoftware/api/TestMaker.java
@@ -12,8 +12,8 @@ public class TestMaker {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Welcome to the TestMaker program!");
-        System.out.println("What would you like to call this test?");
+        log("Welcome to the TestMaker program!");
+        log("What would you like to call this test?");
         String testName = input.next();
 
         boolean isRunning = true;
@@ -57,34 +57,34 @@ public class TestMaker {
     }
 
     private static void displayExit() {
-        System.out.println("\nTest saved.\n" +
+        log("\nTest saved.\n" +
                 "Goodbye!");
     }
 
     private static void displayMenu() {
-        System.out.println("What would you like to do?");
-        System.out.println("\t 1) add a multiple-choice question");
-        System.out.println("\t 2) add a true/false question");
-        System.out.println("\t 3) add a fill-in-the-blank question");
-        System.out.println("\t 4) add short answer question");
-        System.out.println("\t 5) remove a question");
-        System.out.println("\t 6) exit program");
-        System.out.print("Your choice: ");
+        log("What would you like to do?");
+        log("\t 1) add a multiple-choice question");
+        log("\t 2) add a true/false question");
+        log("\t 3) add a fill-in-the-blank question");
+        log("\t 4) add short answer question");
+        log("\t 5) remove a question");
+        log("\t 6) exit program");
+        logger("Your choice: ");
     }
 
     public static IQuestion buildMultipleChoiceQuestion(Scanner input, IQuestionFactory questionFactory) {
         ArrayList<String> questions = new ArrayList<>();
-        System.out.println("What is your multiple-choice question?");
+        log("What is your multiple-choice question?");
         String question = input.next() + input.nextLine();
-        System.out.println();
+        log("");
 
         String[] ordinals = { "first", "second", "third", "fourth and last" };
         String[] answers = new String[4];
         for (int i = 0; i < 4; i++) {
-            System.out.print("Please enter your " + ordinals[i] + " choice: ");
+            logger("Please enter your " + ordinals[i] + " choice: ");
             answers[i] = input.next();
         }
-        System.out.println("What choice was the answer? (Enter #1-4): ");
+        log("What choice was the answer? (Enter #1-4): ");
         int i = input.nextInt();
         int theAnswer = i - 1;
 
@@ -96,10 +96,10 @@ public class TestMaker {
 
     public static IQuestion buildTrueFalseQuestion(Scanner input, IQuestionFactory questionFactory) {
 
-        System.out.println("What is your True/False question?");
+        log("What is your True/False question?");
         String question = input.next() + input.nextLine();
 
-        System.out.println("What is the answer? (Please enter exactly true or false)");
+        log("What is the answer? (Please enter exactly true or false)");
         boolean theAnswer = input.nextBoolean();
 
         return questionFactory.makeTrueFalse(question, theAnswer);
@@ -107,10 +107,10 @@ public class TestMaker {
 
     public static IQuestion buildFillInTheBlankQuestion(Scanner input, IQuestionFactory questionFactory) {
 
-        System.out.println("What is your fill in the blank question?");
+        log("What is your fill in the blank question?");
         String question = input.next() + input.nextLine();
 
-        System.out.println("What is the answer? Please separate answers with a comma.");
+        log("What is the answer? Please separate answers with a comma.");
         String fillInBlankAnswers = input.next();
         String[] answers = fillInBlankAnswers.split(",");
 
@@ -119,17 +119,25 @@ public class TestMaker {
 
    public static IQuestion buildShortAnswerQuestion(Scanner input, IQuestionFactory questionFactory) {
 
-        System.out.println("What is your short answer question?");
+        log("What is your short answer question?");
         String question = input.next() + input.nextLine();
 
-        System.out.println("How many keywords does your short answer question have?");
+        log("How many keywords does your short answer question have?");
         int keywords = Integer.parseInt(input.nextLine());
 
         String arrayOfKeywords[] = new String[keywords];
         for (int i = 0; i < arrayOfKeywords.length; i++) {
-            System.out.print("What is a keyword in your short answer question?" + " : ");
+            logger("What is a keyword in your short answer question?" + " : ");
             arrayOfKeywords[i] = input.nextLine();
         }
         return questionFactory.makeShortAnswer(question, arrayOfKeywords);
+    }
+
+    public static void log(String m) {
+        System.out.println(m);
+    }
+
+    public static void logger(String m) {
+        System.out.print(m);
     }
 }
