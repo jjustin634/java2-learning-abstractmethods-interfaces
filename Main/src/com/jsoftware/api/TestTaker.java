aca90016a35291eff9ccd2e23dbf72bd141cf0ac
Justin Johnson <jjustin634@gmail.com>
2018-11-13 10:01:48 -0600

    added and set up my TestTaker class for the user

---
+++ Main/src/com/jsoftware/api/TestTaker.java
@@ -0,0 +1,14 @@
+package com.jsoftware.api;
+
+public class TestTaker {
+    public static void main(String[] args) {
+
+        log("Welcome to the TestTaker program!");
+        log("What test would you like to take? ");
+        log("Test loaded successful!");
+    }
+
+    public static void log(String m) {
+        System.out.println(m);
+    }
+}
