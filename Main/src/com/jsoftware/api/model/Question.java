1ae9204745e8995739c7d60cf67878fad4c74715
Justin Johnson <jjustin634@gmail.com>
2018-11-13 19:31:23 -0600

    added a return statement to return a question

--- Main/src/com/jsoftware/api/model/Question.java
+++ Main/src/com/jsoftware/api/model/Question.java
@@ -3,14 +3,12 @@ package com.jsoftware.api.model;
 public class Question {
     private String question;
 
-
     public Question(String question) {
         this.question = question;
     }
 
     public String getQuestion() {
-
-        return null;
+        return this.question;
     }
 
 }
