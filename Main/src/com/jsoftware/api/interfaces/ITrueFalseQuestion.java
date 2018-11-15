3bcde6831767999b67408cb2d5415a089b50161b
Justin Johnson <jjustin634@gmail.com>
2018-11-15 13:02:28 -0600

    removed comments in the TrueFalseQuestion interface for the class information

--- Main/src/com/jsoftware/api/interfaces/ITrueFalseQuestion.java
+++ Main/src/com/jsoftware/api/interfaces/ITrueFalseQuestion.java
@@ -1,15 +1,5 @@
 package com.jsoftware.api.interfaces;
 
-import com.jsoftware.api.interfaces.IQuestion;
-
 public interface ITrueFalseQuestion extends IQuestion {
 	public boolean checkAnswer(boolean answer);
 }
-
-
-
-/**
- * Check if the answer is correct.
- * @param answer The user's answer as as a boolean
- * @return true if correct.
- */
