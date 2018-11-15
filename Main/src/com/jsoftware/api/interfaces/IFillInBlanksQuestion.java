796bc64e1401f656a23be43861e9c066f53b819a
Justin Johnson <jjustin634@gmail.com>
2018-11-15 13:01:27 -0600

    Removed comment to make in the FillInTheBlanks Class

--- Main/src/com/jsoftware/api/interfaces/IFillInBlanksQuestion.java
+++ Main/src/com/jsoftware/api/interfaces/IFillInBlanksQuestion.java
@@ -3,10 +3,3 @@ package com.jsoftware.api.interfaces;
 public interface IFillInBlanksQuestion extends IQuestion {
 	public boolean checkAnswer(String[] keywords);
 }
-
-/**
- * A fill in the blank question may contain multiple blanks.
- * Each blank must match each answer.
- * @param keywords An array of answer the user provides.
- * @return
- */
