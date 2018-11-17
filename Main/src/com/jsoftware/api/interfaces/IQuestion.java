decb690d87694bcb458300a6fabf56f80700c284
Justin Johnson <jjustin634@gmail.com>
2018-11-17 14:57:06 -0600

    removed unnecessary comments

--- Main/src/com/jsoftware/api/interfaces/IQuestion.java
+++ Main/src/com/jsoftware/api/interfaces/IQuestion.java
@@ -3,18 +3,3 @@ package com.jsoftware.api.interfaces;
 public interface  IQuestion {
 	String getQuestion();
 }
-
-/**
- * Return a formatted question as a string.  It should include options if
- * the question is a multiple choice or true/false question.
- *
- * Multiple choice example
- * Which of the following is not a primitive type in Java?
- *    1)  double
- *    2)  int
- *    3)  String
- *    4)  boolean
- *
- * True false example:
- * int is not a primitive data type in Java.  True/False?
- */
