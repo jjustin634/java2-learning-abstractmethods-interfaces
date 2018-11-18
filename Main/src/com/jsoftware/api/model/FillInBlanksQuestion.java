9c4127897198e256ecdb641a1144d552a948b183
Justin Johnson <jjustin634@gmail.com>
2018-11-17 18:30:20 -0600

    Added a getQuestion method to my FillInBlanksQuestion class

--- Main/src/com/jsoftware/api/model/FillInBlanksQuestion.java
+++ Main/src/com/jsoftware/api/model/FillInBlanksQuestion.java
@@ -2,7 +2,7 @@ package com.jsoftware.api.model;
 
 import com.jsoftware.api.interfaces.IFillInBlanksQuestion;
 
-public abstract class FillInBlanksQuestion implements IFillInBlanksQuestion {
+public class FillInBlanksQuestion implements IFillInBlanksQuestion {
     private String question;
     private String[] answers;
 
@@ -17,5 +17,9 @@ public abstract class FillInBlanksQuestion implements IFillInBlanksQuestion {
         } else {
             return false;
         }
+    }
+
+    public String getQuestion() {
+        return this.question;
     }
 }
