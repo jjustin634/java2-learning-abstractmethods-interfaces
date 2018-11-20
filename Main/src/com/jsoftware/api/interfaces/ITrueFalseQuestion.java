908ed339a6a0644a48a4c9b0f94b34ea501685c6
Justin Johnson <jjustin634@gmail.com>
2018-11-20 13:19:07 -0600

    removing public identifier

--- Main/src/com/jsoftware/api/interfaces/ITrueFalseQuestion.java
+++ Main/src/com/jsoftware/api/interfaces/ITrueFalseQuestion.java
@@ -1,5 +1,5 @@
 package com.jsoftware.api.interfaces;
 
 public interface ITrueFalseQuestion extends IQuestion {
-	public boolean checkAnswer(boolean answer);
+	boolean checkAnswer(boolean answer);
 }
