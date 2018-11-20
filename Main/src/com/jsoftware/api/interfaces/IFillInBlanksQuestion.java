1fbcc4e7f1ec69207b61657187d161116cd0825e
Justin Johnson <jjustin634@gmail.com>
2018-11-20 13:05:30 -0600

    removing the public identifier

--- Main/src/com/jsoftware/api/interfaces/IFillInBlanksQuestion.java
+++ Main/src/com/jsoftware/api/interfaces/IFillInBlanksQuestion.java
@@ -1,5 +1,5 @@
 package com.jsoftware.api.interfaces;
 
 public interface IFillInBlanksQuestion extends IQuestion {
-	public boolean checkAnswer(String[] keywords);
+	boolean checkAnswer(String[] keywords);
 }
