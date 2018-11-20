bde130d37e1e86009b82f279e57083152a88e369
Justin Johnson <jjustin634@gmail.com>
2018-11-20 13:21:13 -0600

    updating MultipleChoiceQuestion class - adding a print method that enables a specific question to be printed

--- Main/src/com/jsoftware/api/model/MultipleChoiceQuestion.java
+++ Main/src/com/jsoftware/api/model/MultipleChoiceQuestion.java
@@ -1,12 +1,13 @@
 package com.jsoftware.api.model;
 
 import com.jsoftware.api.interfaces.IMultipleChoiceQuestion;
+import java.io.Serializable;
+import static com.jsoftware.api.model.Logger.log;
 
-public class MultipleChoiceQuestion implements IMultipleChoiceQuestion {
+public class MultipleChoiceQuestion implements IMultipleChoiceQuestion, Serializable {
     private String question;
     private String[] answers;
     private int correctAnswer;
-
 
     public MultipleChoiceQuestion(String question, String[] answers, int correctAnswer) {
         this.question = question;
@@ -20,6 +21,17 @@ public class MultipleChoiceQuestion implements IMultipleChoiceQuestion {
 
     public String getQuestion() {
         return this.question;
+    }
+
+    public void print() {
+        log(this.question);
+
+        int n;
+        for (int i = 0; i < this.answers.length; i++) {
+            n = i + 1;
+            log("\t" + n  + ") ", true);
+            log(this.answers[i]);
+        }
     }
 }
 
