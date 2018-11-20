831d0c1b07c3c871044c234728e01772bda29343
Justin Johnson <jjustin634@gmail.com>
2018-11-20 13:18:10 -0600

    removing white space

--- Main/src/com/jsoftware/api/interfaces/IQuestionFactory.java
+++ Main/src/com/jsoftware/api/interfaces/IQuestionFactory.java
@@ -3,18 +3,10 @@ package com.jsoftware.api.interfaces;
 import java.io.IOException;
 
 public interface IQuestionFactory {
-
-	public IQuestion makeMultipleChoice(String question, String[] choices, int answer);
-
-	public IQuestion makeTrueFalse(String question, boolean answer);
-
-	public IQuestion makeFillInBlank(String question, String [] answers);
-
-	public IQuestion makeShortAnswer(String question, String[] keywords);
-
-	public IQuestionSet load(String filename) throws IOException;
-
-	public boolean save(IQuestionSet testSet, String filename);
-
-	public IQuestionSet makeEmptyQuestionSet();
+	IQuestion makeMultipleChoice(String question, String[] choices, int answer);
+	IQuestion makeTrueFalse(String question, boolean answer);
+	IQuestion makeFillInBlank(String question, String [] answers);
+	IQuestion makeShortAnswer(String question, String[] keywords);
+	IQuestionSet load(String filename) throws IOException;
+	boolean save(IQuestionSet testSet, String filename);
 }
