cb14e8870db839cc6aa8844b20673ea1dd792296
Justin Johnson <jjustin634@gmail.com>
2018-11-20 13:18:59 -0600

    removing public identifier

--- Main/src/com/jsoftware/api/interfaces/IShortAnswerQuestion.java
+++ Main/src/com/jsoftware/api/interfaces/IShortAnswerQuestion.java
@@ -1,5 +1,5 @@
 package com.jsoftware.api.interfaces;
 
 public interface IShortAnswerQuestion extends IQuestion {
-	public boolean checkAnswer(String answer);
+	boolean checkAnswer(String answer);
 }
