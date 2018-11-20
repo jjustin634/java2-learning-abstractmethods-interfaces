107b6dc99a2285df1c20a0f4da3357c151390353
Justin Johnson <jjustin634@gmail.com>
2018-11-20 13:25:11 -0600

    updated QuestionSet - randomSample method adds all of the users questions into an ArrayList shuffles them using Collections and will allow the user to take a sample question with a maximum of 2 random questions from their test. If the user only has 1 question it will only add 1 question to the sample test.

--- Main/src/com/jsoftware/api/model/QuestionSet.java
+++ Main/src/com/jsoftware/api/model/QuestionSet.java
@@ -2,22 +2,35 @@ package com.jsoftware.api.model;
 
 import com.jsoftware.api.interfaces.IQuestion;
 import com.jsoftware.api.interfaces.IQuestionSet;
-
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
+import static com.jsoftware.api.model.Logger.log;
 
-public class QuestionSet implements IQuestionSet {
+public class QuestionSet implements IQuestionSet, Serializable {
     private ArrayList<IQuestion> questions;
 
     public QuestionSet() {
         this.questions = new ArrayList<>();
     }
 
-    public IQuestionSet emptyTestSet() {
-        return new QuestionSet();
-    }
-
     public IQuestionSet randomSample(int size) {
-        return null;
+        if (size > questions.size()) {
+            size = questions.size();
+        }
+
+        ArrayList<Integer> questionNumbers = new ArrayList<>();
+        for (int i = 0; i < questions.size(); i++) {
+            questionNumbers.add(i);
+        }
+
+        Collections.shuffle(questionNumbers);
+
+        IQuestionSet sample = new QuestionSet();
+        for (int i = 0; i < size; i++) {
+            sample.add(this.getQuestion(questionNumbers.get(i)));
+        }
+        return sample;
     }
 
     public boolean add(IQuestion question) {
@@ -34,5 +47,14 @@ public class QuestionSet implements IQuestionSet {
 
     public int size() {
         return this.questions.size();
+    }
+
+    public void print() {
+        IQuestion question;
+        for (int i = 0; i < this.questions.size(); i++) {
+            question = this.questions.get(i);
+            log("Question " + i + ") ", true);
+            question.print();
+        }
     }
 }
